c432b14ab2057987078dd71527816331576707a5
Stephen Cronin <stephen.cronin@reaktor.com>
2019-05-29 09:30:42 -0400

    Animate 2 lines

--- src/components/VisualizationB/index.js
+++ src/components/VisualizationB/index.js
@@ -3,15 +3,33 @@ import paper from 'paper';
 import './index.css';
 
 const POINTS = 10;
-const NOISE_HEIGHT = 100;
+const NOISE_HEIGHT = 20;
+const COLORS = {
+  darkBlue: '#0073E5',
+  lightBlue: '#00E7D8',
+}
+
+const sinNoise = seed => {
+  const SPEED1 = 200;
+  const SPEED2 = 100;
+  const sin1 = Math.sin(seed / SPEED1) * NOISE_HEIGHT;
+  const sin2 = Math.sin(seed / SPEED2) * sin1;
+  return sin2;
+}
 
 class PoolPaper {
   constructor(props) {
     this.wrapper = props.wrapper;
-    this.path = null;
+    this.path1 = null;
+    this.path2 = null;
+    this.layer1 = null;
+    this.layer2 = null;
+
+    paper.settings.applyMatrix = false;
 
     this.initializeCanvas();
-    this.initializePath();
+    this.initializeLayers();
+    this.drawWaterPaths();
     paper.view.onFrame = this.onFrame.bind(this);
   }
 
@@ -27,37 +45,53 @@ class PoolPaper {
     paper.view.translate([0, height/2]);
   };
 
-  initializePath() {
+  initializeLayers() {    
+    this.layer1 = new paper.Layer();
+    this.layer2 = new paper.Layer();
+    this.layer2.translate(new paper.Point(10, 10));
+  }
+
+  drawWaterPaths() {
     const { width } = paper.view.size;
     const center = paper.view.center;
-    this.path = new paper.Path();
-    this.path.strokeColor = 'black';
+
+    this.path1 = new paper.Path();
+    this.path1.strokeColor = COLORS.darkBlue;
+    this.path1.strokeWidth = 2;
   
     // first point
-    this.path.add([0, 0]);
+    this.path1.add([0, 0]);
   
     // middle points
     for (var i = 1; i < POINTS; i++) {
       const point = new paper.Point(width / POINTS * i, center.y);
-      this.path.add(point);
+      this.path1.add(point);
     }
   
     // last point
-    this.path.add([width, 0]);
+    this.path1.add([width, 0]);
   
     // DEBUG: Show points on path
-    this.path.fullySelected = true;
+    // this.path1.fullySelected = true;
+
+    this.path2 = this.path1.clone();
+    this.path2.strokeColor = COLORS.lightBlue;
+
+    this.layer1.addChild(this.path1);
+    this.layer2.addChild(this.path2);
   }
 
   onFrame(event) {
     for (var i = 1; i < POINTS; i++) {
       const sinSeed = event.count + (i + i % 10) * 100;
-      const sinHeight = Math.sin(sinSeed / 200) * NOISE_HEIGHT;
-      const yPos = Math.sin(sinSeed / 100) * sinHeight;
-      this.path.segments[i].point.y = yPos;
+      const yPos = sinNoise(sinSeed);
+      this.path1.segments[i].point.y = yPos;
+      this.path2.segments[i].point.y = yPos;
     }
-    
-    this.path.smooth({ type: 'continuous' });
+     
+    // Apply smooth
+    this.path1.smooth({ type: 'continuous' });
+    this.path2.smooth({ type: 'continuous' });
   }
 }
 
@@ -71,7 +105,7 @@ class Visualization extends Component {
 
   componentDidMount() {
     const wrapper = this.wrapper.current;
-    new PoolPaper({wrapper});
+    new PoolPaper({ wrapper });
   }
 
   render() {
