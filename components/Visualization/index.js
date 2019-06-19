022b8e999941b80411fdabb5e4c924677a2df98f
Jonathan Dahan <hi@jonathan.is>
2019-06-19 13:41:45 -0400

    Remove range reducers

--- components/Visualization/index.js
+++ components/Visualization/index.js
@@ -6,7 +6,7 @@ import { getSampleAtTimestamp, constrain } from '../../helpers/data'
 import './index.css' /* eslint-disable-line import/no-unassigned-import */
 
 const Visualization = ({ noaaData, stationData }) => {
-  const initialRange = constrain({
+  const range = constrain({
     start: Date.parse(noaaData[0].t),
     end: Date.parse(noaaData[noaaData.length - 1].t)
   },
@@ -14,9 +14,8 @@ const Visualization = ({ noaaData, stationData }) => {
     start: stationData.samples[0][0],
     end: stationData.samples[stationData.samples.length - 2][0]
   })
-  const [range, constrainRange] = useReducer(constrain, initialRange)
 
-  const [timestamp, setTimestamp] = useState(initialRange.end)
+  const [timestamp, setTimestamp] = useState(range.end)
 
   const initialSample = getSampleAtTimestamp({}, {
     noaaData,
@@ -24,33 +23,6 @@ const Visualization = ({ noaaData, stationData }) => {
     timestamp
   })
   const [sample, updateSample] = useReducer(getSampleAtTimestamp, initialSample)
-
-  // Constrain the date range when we get new noaa data
-  useEffect(() => {
-    if (noaaData && noaaData.length > 0) {
-      constrainRange({
-        start: Date.parse(noaaData[0].t),
-        end: Date.parse(noaaData[noaaData.length - 1].t)
-      })
-    }
-  }, [noaaData])
-
-  // Constrain the date range when we get new station data
-  useEffect(() => {
-    if (stationData && stationData.samples && stationData.samples.length > 1) {
-      constrainRange({
-        start: stationData.samples[0][0],
-        end: stationData.samples[stationData.samples.length - 2][0]
-      })
-    }
-  }, [stationData])
-
-  // Make sure timestamp is within our range
-  useEffect(() => {
-    setTimestamp(timestamp =>
-      Math.min(Math.max(timestamp, range.start), range.end)
-    )
-  }, [range])
 
   const wrapper = useRef(null)
   const paperAnimation = useRef(null)
@@ -61,7 +33,7 @@ const Visualization = ({ noaaData, stationData }) => {
       stationData,
       timestamp
     })
-  }, [noaaData, stationData, timestamp])
+  }, [timestamp])
 
   const changeTimestamp = () => {
     setTimestamp(range.start + Math.floor(Math.random() * (range.end - range.start)))
@@ -76,7 +48,7 @@ const Visualization = ({ noaaData, stationData }) => {
   }
 
   useEffect(initPaperAnimation, [])
-  useEffect(updatePaperAnimation, [paperAnimation, sample])
+  useEffect(updatePaperAnimation, [sample])
 
   return (
     <div className='visualization'>
