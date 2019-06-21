1c5a76423eea7c725e13ce0384fd2ba68bd7239c
Stephen Cronin <stephen.cronin@reaktor.com>
2019-06-21 16:53:01 -0400

    Make slider min and max constants

--- components/DataRangePicker/index.js
+++ components/DataRangePicker/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Slider from 'rc-slider'
+import RcSlider from 'rc-slider'
 import dayjs from 'dayjs'
 import { throttle } from 'lodash'
 import { formatTime } from '../../helpers/date'
@@ -7,7 +7,9 @@ import { scale } from '../../helpers/data'
 import './index.css'
 import 'rc-slider/assets/index.css'
 
-const RcHandle = Slider.Handle
+const SLIDER_MIN = 0
+const SLIDER_MAX = 100
+const RcHandle = RcSlider.Handle
 
 const handle = ({ atStart, atEnd, label, value, dragging, index, ...restProps }) => {
   const visible = !atStart && !atEnd
@@ -34,13 +36,13 @@ class DataRangePicker extends React.Component {
   // Scale from Slider value to our timestamp
   scaleFrom (value) {
     const { range: { start, end } } = this.props
-    return scale(value, 0, 100, end, start)
+    return scale(value, SLIDER_MIN, SLIDER_MAX, end, start)
   }
 
   // Scale our timestamp to our Slider value
   scaleTo (value) {
     const { range: { start, end } } = this.props
-    return scale(value, end, start, 0, 100)
+    return scale(value, end, start, SLIDER_MIN, SLIDER_MAX)
   }
 
   render () {
@@ -61,7 +63,9 @@ class DataRangePicker extends React.Component {
       <div className='data-range-picker'>
         <label className='data-range-picker__label'>{formatTime(endDateDiff)}</label>
         <div className='data-range-picker__slider-container'>
-          <Slider
+          <RcSlider
+            min={SLIDER_MIN}
+            max={SLIDER_MAX}
             value={this.scaleTo(timestamp)}
             onChange={value => this.onChange(value)}
             handle={(props) =>
