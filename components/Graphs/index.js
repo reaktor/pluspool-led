7fcf486bd05f771753b2459c6a68ede366611961
Stephen Cronin <stephen.cronin@reaktor.com>
2019-07-02 10:25:47 -0400

    Date filter adjustments

--- components/Graphs/index.js
+++ components/Graphs/index.js
@@ -2,84 +2,65 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { before } from '../../helpers/data'
 import Chart from '../Chart'
+import GraphsDateFilter from '../GraphsDateFilter'
 import './index.css'
 
-const Choices = ({ name, choices, onChange }) => {
-  const innerOnChange = ({ target: { value } }) => onChange(value)
+const units = ['day', 'week', 'month', 'year']
 
-  const choiceToLabel = choice => {
-    switch (choice) {
-      case 'day': return 'Today'
-      case 'week': return 'Past Week'
-      case 'month': return 'Past Month'
-      case 'year': return 'Past Year'
-      default: return 'Select Date Range'
-    }
+const graphs = [
+  {
+    header: 'Water Direction',
+    unit: 'degrees',
+    y: 'd'
+  },
+  {
+    header: 'Water Speed',
+    unit: 'knots',
+    y: 's'
+  },
+  {
+    header: 'Percent Oxygen',
+    unit: '%',
+    y: 'Percent Oxygen_SDI_0_10_%'
+  },
+  {
+    header: 'Salinity',
+    unit: 'PPT',
+    y: 'Salinity_SDI_0_4_ppt'
+  },
+  {
+    header: 'Turbidity',
+    unit: 'NTU',
+    y: 'Turbidity_SDI_0_8_NTU'
+  },
+  {
+    header: 'pH',
+    unit: 'pH',
+    y: 'pH mV_SDI_0_7_V'
+  }
+]
+
+const Graphs = ({ samples }) => {
+  if (!samples) return null
+
+  const data = samples
+  const [activeUnit, setActiveUnit] = useState('month')
+  const [domain, setDomain] = useState([Date.now(), before(activeUnit)])
+
+  const setSpan = unit => setDomain([Date.now(), before(unit)])
+  const filterOnClick = unit => {
+    setActiveUnit(unit)
+    setSpan(unit)
   }
 
   return (
-    <span className='choices'>
-      { choices.map(choice => (
-        <Choice name={name} choice={choice} onChange={innerOnChange} label={choiceToLabel(choice)} />
-      )) }
-    </span>
-  )
-}
-const Choice = ({ name, choice, label, onChange }) => {
-  return (
     <>
-      <input type='radio' id={choice} name={name} value={choice} onChange={onChange} />
-      <label for={choice}>{label}</label>
-    </>
-  )
-}
-
-const Graphs = ({ noaaData, stationData, samples }) => {
-  if (!noaaData || !stationData) return null
-
-  const data = samples
-  const [domain, setDomain] = useState([Date.now(), before('month')])
-
-  const setSpan = unit => setDomain([Date.now(), before(unit)])
-
-  const graphs = [
-    {
-      header: 'Water Direction',
-      unit: 'degrees',
-      y: 'd'
-    },
-    {
-      header: 'Water Speed',
-      unit: 'knots',
-      y: 's'
-    },
-    {
-      header: 'Percent Oxygen',
-      unit: '%',
-      y: 'Percent Oxygen_SDI_0_10_%'
-    },
-    {
-      header: 'Salinity',
-      unit: 'PPT',
-      y: 'Salinity_SDI_0_4_ppt'
-    },
-    {
-      header: 'Turbidity',
-      unit: 'NTU',
-      y: 'Turbidity_SDI_0_8_NTU'
-    },
-    {
-      header: 'pH',
-      unit: 'pH',
-      y: 'pH mV_SDI_0_7_V'
-    }
-  ]
-
-  const choices = ['day', 'week', 'month', 'year']
-
-  return (
-    <>
-      <Choices choices={choices} onChange={setSpan} name='span' />
+      <GraphsDateFilter
+        units={units}
+        activeUnit={activeUnit}
+        onChange={filterOnClick}
+        name='span'
+      />
       <div className='graphs'>
         {graphs.map(graph => (
           <Chart x='t' domain={domain} data={data} {...graph} />
