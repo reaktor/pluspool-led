b4d267ff911307f2bbde86a2208e7f1a93fd3959
Stephen Cronin <stephen.cronin@reaktor.com>
2019-07-03 14:23:43 -0400

    Fix databar values

--- components/Databar/index.js
+++ components/Databar/index.js
@@ -5,7 +5,7 @@ import DatabarItem from '../DatabarItem'
 import { svgIcons } from '../../helpers/icons'
 import './index.css'
 
-const displayedColumns = [
+const displayedSlugs = [
   'oxygen',
   'salinity',
   'turbidity',
@@ -18,17 +18,18 @@ const Databar = ({ onItemClick, sample }) => {
   return (
     <div className='databar'>
       <div className='databar__wrapper'>
-        {displayedColumns
-          .filter(column => dataValues[column].column in sample)
+        {displayedSlugs
+          .filter(slug => dataValues[slug].column in sample)
           .map(
             slug => {
               const data = dataValues[slug]
               const icon = svgIcons[slug]
+              const { column } = data
               return (
                 <DatabarItem
                   key={slug}
                   icon={icon && icon()}
-                  value={sample[slug]}
+                  value={sample[column]}
                   onClick={() => onItemClick(slug)}
                   {...data}
                 />
