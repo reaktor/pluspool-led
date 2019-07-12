43b46fe0a6bcd06aea2f3993a48a7b285d93f1cc
Jonathan Dahan <hi@jonathan.is>
2019-07-12 17:46:01 -0400

    add clientside updates

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import Databar from '../components/Databar'
@@ -9,17 +9,36 @@ import Tooltip from '../components/Tooltip'
 import { fetchSamplesData } from '../helpers/data'
 import './index.css'
 
-function IndexPage ({ samples }) {
+const getRange = samples => ({
+  start: samples[0].noaaTime,
+  end: samples[samples.length - 1].noaaTime
+})
+
+const getLatestSample = samples => samples[0]
+
+const getSampleAtTimestamp = (samples, timestamp) => {
+  return samples.find(({ noaaTime }) => noaaTime >= timestamp)
+}
+
+function IndexPage ({ samples: initialSamples }) {
   const [tooltipOpen, setTooltipOpen] = useState(false)
-  const [tooltipSlug, setTooltipSlug] = useState(null)
+  const [tooltipSlug, setTooltipSlug] = useState()
+  const [samples, setSamples] = useState(initialSamples)
 
-  const range = {
-    start: samples[0].noaaTime,
-    end: samples[samples.length - 1].noaaTime
-  }
-
+  const [range, setRange] = useState(getRange(initialSamples))
   const [timestamp, setTimestamp] = useState(range.end)
-  const sample = samples.find(({ noaaTime }) => noaaTime >= timestamp)
+  const [sample, setSample] = useState(getSampleAtTimestamp(samples,timestamp))
+
+  useEffect(() => setSample(getSampleAtTimestamp(samples, timestamp)), [samples, timestamp])
+
+  useEffect(() => setTimestamp(range.end), [range])
+
+  useEffect(() => setRange(getRange(samples)), [samples])
+
+  useEffect(() => fetchSamplesData().then(({date, samples}) => {
+    console.log(date)
+    setSamples(samples)
+  }), [])
 
   return (
     <main className='page' data-template='index'>
