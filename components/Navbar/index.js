33263efb6c4d5b6fe5c1710c7e5b3d0b4fbd2f53
Stephen Cronin <stephen.cronin@reaktor.com>
2019-07-12 16:45:35 -0400

    Add nav links

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import Link from 'next/link'
+import Arrow from '../../icons/Arrow'
 import './index.css'
 
 const Navbar = () => (
   <div className='navbar'>
     <h1 className='navbar__title'>
       <Link href='/'>
-        <a className='navbar__title__link'>+ <span className='navbar__title__label'>Pool Water Quality Dashboard</span></a>
+        <a className='navbar__title__link'><strong>+POOL</strong> Water Quality Dashboard</a>
       </Link>
     </h1>
-    <div className='navbar__link-out'>
-      <span className='navbar__link-out__label'>Want to learn more and get involved?{' '}</span>
-      <a className='navbar__link-out__link' href='https://pluspool.org' target='_BLANK' rel='noopener'>pluspool.org</a>
+    <div>
+      <nav className='navbar__nav'>
+        <Link href='/'>
+          <a className='navbar__nav__link'>Dashboard</a>
+        </Link>
+        <Link href='/data'>
+          <a className='navbar__nav__link'>Data</a>
+        </Link>
+        <Link href='https://pluspool.org'>
+          <a className='navbar__nav__link'>What is +POOL? <Arrow /></a>
+        </Link>
+      </nav>
     </div>
   </div>
 )
--- pages/data.js
+++ pages/data.js
@@ -9,7 +9,7 @@ function DataPage (props) {
   return (
     <main className='page' data-template='data'>
       <Head>
-        <title>+Pool Light Installation</title>
+        <title>+POOL Lights</title>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         <link rel='shortcut icon' href='/static/favicon.ico' />
         <link
