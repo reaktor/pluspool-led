d0350d71a31a24af600a330c38759b481e10a786
Luke Grecki <luke.grecki@reaktor.com>
2019-07-18 16:41:06 -0400

    Added destructuring

--- components/SocialMetaTags/index.js
+++ components/SocialMetaTags/index.js
@@ -1,18 +1,18 @@
-const SocialMetaTags = (props) => (
+const SocialMetaTags = ({url, title, description, image_url}) => (
   <>
     {/* Open Graph meta tags */}
     <meta property='og:type' content='website' />
-    <meta property='og:url' content={props.url} />
-    <meta property='og:title' content={props.title} />
-    <meta property='og:description' content={props.description} />
-    <meta property='og:image' content={props.image_url} />
+    <meta property='og:url' content={url} />
+    <meta property='og:title' content={title} />
+    <meta property='og:description' content={description} />
+    <meta property='og:image' content={image_url} />
 
     {/* Twitter meta tags */}
     <meta property='twitter:card' content='summary_large_image' />
-    <meta property='twitter:url' content={props.url} />
-    <meta property='twitter:title' content={props.title} />
-    <meta property='twitter:description' content={props.description} />
-    <meta property='twitter:image' content={props.image_url} />
+    <meta property='twitter:url' content={url} />
+    <meta property='twitter:title' content={title} />
+    <meta property='twitter:description' content={description} />
+    <meta property='twitter:image' content={image_url} />
   </>
 )
 
