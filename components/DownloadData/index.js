0f7f9a032e4a81bdd8e4c889fc245d488c10cd41
Luke Grecki <luke.grecki@reaktor.com>
2019-07-23 14:27:24 -0400

    Clean up jsx

--- components/DownloadData/index.js
+++ components/DownloadData/index.js
@@ -2,15 +2,13 @@ import DownloadIcon from '../../icons/DownloadIcon'
 import { ENDPOINTS } from '../../helpers/constants'
 import './index.css'
 
-const DownloadData = () => {
-  return (
-    <div className='download-data'>
-      <span className='download-data__label'>Download Data</span>
-      <a href={ENDPOINTS.samples}>
-        <DownloadIcon />
-      </a>
-    </div>
-  )
-}
+const DownloadData = () => (
+  <div className='download-data'>
+    <span className='download-data__label'>Download Data</span>
+    <a href={ENDPOINTS.samples}>
+      <DownloadIcon />
+    </a>
+  </div>
+)
 
 export default DownloadData
