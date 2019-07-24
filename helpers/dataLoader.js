e2be570f5875ee99994f0d7153633fc1a2714fd4
Jonathan Dahan <github@jonathan.is>
2019-07-24 11:06:48 -0400

    Switch to gzip (#73)

--- helpers/dataLoader.js
+++ helpers/dataLoader.js
@@ -9,7 +9,7 @@ const dataFetchParams = {
   mode: 'cors',
   cache: 'no-cache',
   credentials: 'same-origin',
-  headers: { 'Content-Type': 'application/json' },
+  headers: { 'Content-Type': 'application/json', 'Accept-Encoding': 'gzip' },
   referrer: 'no-referrer'
 }
 
