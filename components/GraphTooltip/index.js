0351ec57ff39ded1d142e537f8627410aac56a3a
Jonathan Dahan <hi@jonathan.is>
2019-09-09 16:03:32 -0400

    No longer subtract 5 hours

--- components/GraphTooltip/index.js
+++ components/GraphTooltip/index.js
@@ -5,8 +5,9 @@ import './index.css'
 const GraphTooltip = (props) => {
   const { unit, label, overlayGraph, data, point: { data: { x, y }, index } } = props
   const overlayData = overlayGraph && data[index][overlayGraph.y]
+
   return (<div className='graph-tooltip'>
-    <div className='graph-tooltip__date'>{dayjs(x).subtract(5, 'hours').format('MMM DD YYYY HH:mm')}</div>
+    <div className='graph-tooltip__date'>{dayjs(x).format('MMM DD YYYY HH:mm Z')}</div>
     <div className='graph-tooltip__data'>
       {label}: {y.toFixed(2)} {unit}
     </div>
