e4265645e6c762756af7d362668740024595b172
Stephen Cronin <stephen.cronin@reaktor.com>
2019-09-10 17:32:42 -0400

    Fix og:image_url

--- content/social.js
+++ content/social.js
@@ -1,8 +1,8 @@
 import { BASE_URL } from '../helpers/constants'
-
+console.log(`${BASE_URL}/static/img/home-social-preview.png`)
 export default {
   url: BASE_URL,
   title: '+ POOL Water Quality Data Dashboard',
   description: 'A detailed dashboard for visualizing the components of water quality in the + POOL floating pool in the East River of NYC.',
-  image_url: `${BASE_URL}/static/img/home-social-preview.png`
+  imageUrl: `${BASE_URL}/static/img/home-social-preview.png`
 }
