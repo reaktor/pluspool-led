683b5ff8c61f0ebf63fd835fbb1022950b58d155
Stephen Cronin <stephen.cronin@reaktor.com>
2019-09-10 16:18:18 -0400

    Format GraphTooltip date as local

--- components/GraphTooltip/index.js
+++ components/GraphTooltip/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
 import './index.css'
+
+dayjs.extend(utc)
 
 const GraphTooltip = (props) => {
   const { unit, label, overlayGraph, data, point: { data: { x, y }, index } } = props
   const overlayData = overlayGraph && data[index][overlayGraph.y]
 
   return (<div className='graph-tooltip'>
-    <div className='graph-tooltip__date'>{dayjs(x).format('MMM DD YYYY HH:mm Z')}</div>
+    <div className='graph-tooltip__date'>{dayjs(x).local().format('MMM DD YYYY HH:mm A')}</div>
     <div className='graph-tooltip__data'>
       {label}: {y.toFixed(2)} {unit}
     </div>
