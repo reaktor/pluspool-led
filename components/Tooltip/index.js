2f2b74c6776da7663a8f30b42ff9992496acff16
Stephen Cronin <638332+cronin4392@users.noreply.github.com>
2019-09-12 17:34:05 -0400

    Add disclaimer text to tooltip (#114)

--- components/Tooltip/index.js
+++ components/Tooltip/index.js
@@ -1,19 +1,22 @@
 import React from 'react'
+import Tippy from '@tippy.js/react'
 import Legend from '../Legend'
 import TooltipSource from '../TooltipSource'
 import content from '../../content'
 import Circle from '../../icons/Circle'
+import ExclamationCircle from '../../icons/ExclamationCircle'
+import X from '../../icons/X'
 
 import './index.css'
 
 const Tooltip = ({ closeTooltip, open, slug, sample, sources, units }) => {
-  // Outer div required for CSS transition to occur on first open
+  // We need to render our containing div even when no slug in order for transition in to work
   if (!slug) {
     return <div className='tooltip' data-active={open} />
   }
 
   const [source, unit] = [sources[slug], units[slug]]
-  const { label, legend, transform, color, max } = content.dataPoints[slug]
+  const { label, legend, transform, color, max, disclaimerText } = content.dataPoints[slug]
 
   const value = sample ? transform ? transform(sample[slug]) : sample[slug] : null
 
@@ -26,24 +29,7 @@ const Tooltip = ({ closeTooltip, open, slug, sample, sources, units }) => {
             type='button'
             onClick={closeTooltip}
           >
-            <svg viewBox='0 0 10 10' preserveAspectRatio='none'>
-              <path
-                vector-effect='non-scaling-stroke'
-                fill='transparent'
-                stroke='#000000'
-                stroke-width='1'
-                stroke-linejoin='round'
-                d='M0,0l10,10'
-              />
-              <path
-                vector-effect='non-scaling-stroke'
-                fill='transparent'
-                stroke='#000000'
-                stroke-width='1'
-                stroke-linejoin='round'
-                d='M10,0l-10,10'
-              />
-            </svg>
+            <X />
           </button>
         </div>
         <div className='tooltip__heading'>
@@ -52,6 +38,17 @@ const Tooltip = ({ closeTooltip, open, slug, sample, sources, units }) => {
               <Circle fill={color} />
             </div>}
           <h3 className='tooltip__header'>{label}</h3>
+          {disclaimerText &&
+            <Tippy
+              content={disclaimerText}
+              placement='bottom'
+              maxWidth={285}
+              trigger='click'
+            >
+              <button class='tooltip__disclaimer'>
+                <ExclamationCircle />
+              </button>
+            </Tippy>}
           {value && <span className='tooltip__value'>{value} {unit}</span>}
         </div>
         {legend && (
