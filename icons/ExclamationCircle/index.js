2bfd75eb5ea73fd5b1af5ca7bcd75b0ebc679f6b
Stephen Cronin <stephen.cronin@reaktor.com>
2019-09-13 18:24:33 -0400

    Mobile layout fixes

--- icons/ExclamationCircle/index.js
+++ icons/ExclamationCircle/index.js
@@ -1,15 +1,11 @@
 import React from 'react'
 
 export default () => (
-  <svg width='25px' height='25px' viewBox='0 0 25 25'>
-    <g id='UI' stroke='none' stroke-width='1' fill='none' fill-rule='evenodd'>
-      <g id='A-1-Home-Tooltip' transform='translate(-594.000000, -212.000000)'>
-        <g id='section.left' transform='translate(40.000000, 154.000000)'>
-          <g id='footnote' transform='translate(554.000000, 58.000000)'>
-            <circle id='Oval' fill='#000000' cx='12.5' cy='12.5' r='12.5' />
-            <path d='M11.712,14.24 L11.712,6.64 L13.744,6.64 L13.744,14.24 L11.712,14.24 Z M11.504,19 L11.504,16.456 L13.952,16.456 L13.952,19 L11.504,19 Z' id='!' fill='#FFFFFF' fill-rule='nonzero' />
-          </g>
-        </g>
+  <svg viewBox='0 0 27 27' style={{ margin: '-1px' }}>
+    <g stroke='none' stroke-width='1' fill='none' fill-rule='evenodd'>
+      <g transform='translate(1.000000, 1.000000)'>
+        <circle fill='#000000' cx='12.5' cy='12.5' r='12.5' />
+        <path d='M11.712,14.24 L11.712,6.64 L13.744,6.64 L13.744,14.24 L11.712,14.24 Z M11.504,19 L11.504,16.456 L13.952,16.456 L13.952,19 L11.504,19 Z' fill='#FFFFFF' fill-rule='nonzero' />
       </g>
     </g>
   </svg>
