978217127fe51c4252eb19861a6d2b60cd91756a
Timo Jaaskelainen <timojaask@gmail.com>
2020-06-10 17:16:37 -0400

    Update copy
    We’re no longer showing “Today” data, since the sensors were turned off.

--- content/titleText.js
+++ content/titleText.js
@@ -4,8 +4,8 @@ import TitleTextTooltip from '../components/TitleTextTooltip'
 const tooltipText = 'But even if we say water is great for swimming, we are NOT suggesting that you get in it! This dashboard does not condone unsanctioned swimming and is only an awareness platform. Please don\'t jump in until we have a + POOL!'
 
 export default {
-  introText: <><span>We’re tracking the water in the NYC harbor at Pier 17.</span><span>How is the water today?</span></>,
-  introCta: 'Click here to find out.',
+  introText: <><span>We’re tracking the water in the NYC harbor at Pier 17.</span></>,
+  introCta: 'Click here see the results.',
   bacteriaText: {
     acceptable: <>great for <TitleTextTooltip tooltipText={tooltipText}>swimming</TitleTextTooltip>.</>,
     unacceptablePersist: <>not great for <TitleTextTooltip tooltipText={tooltipText}>swimming</TitleTextTooltip> if levels persist.</>,
