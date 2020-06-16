1a887a3177996e47f10d491829bd8bd4a995740c
Timo Jaaskelainen <timojaask@gmail.com>
2020-06-16 16:25:00 -0400

    Change language to past tense

--- content/titleText.js
+++ content/titleText.js
@@ -4,7 +4,7 @@ import TitleTextTooltip from '../components/TitleTextTooltip'
 const tooltipText = 'But even if we say water is great for swimming, we are NOT suggesting that you get in it! This dashboard does not condone unsanctioned swimming and is only an awareness platform. Please don\'t jump in until we have a + POOL!'
 
 export default {
-  introText: <><span>We’re tracking the water in the NYC harbor at Pier 17.</span></>,
+  introText: <><span>We tracked the water in the NYC harbor at Pier 17.</span></>,
   introCta: 'Click here see the results.',
   bacteriaText: {
     acceptable: <>great for <TitleTextTooltip tooltipText={tooltipText}>swimming</TitleTextTooltip>.</>,
