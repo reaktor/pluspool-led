60edfee0a465a9166c41997174a6e66febf53512
Jonathan Dahan <git@jonathan.is>
2023-02-21 16:01:07 -0500

    Move from static to public directory
    This fixes netlify deploy, hopefully

--- pages/_app.js
+++ pages/_app.js
@@ -33,8 +33,8 @@ const Header = () => (
   <Head>
     <title>{content.social.title}</title>
     <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    <link rel='shortcut icon' href='/static/favicon.ico' />
-    <link rel='icon' href='/static/favicon.ico' />
+    <link rel='shortcut icon' href='/favicon.ico' />
+    <link rel='icon' href='/favicon.ico' />
 
     <meta property='og:type' content='website' />
     <meta property='og:url' content={content.social.url} />
