a25a9b181408aa949a09d266b72337cba4d2f0fa
Amir Kaudinov <amirkaudinov@R3060.local>
2023-04-28 11:03:27 -0600

    feat: updated the sample source to the new CDK controlled s3 bucket

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,5 +1,5 @@
 const ENDPOINTS = {
-  samples:'https://pluspool-east-river-data.s3.us-east-2.amazonaws.com/2020-04-30T00%3A00%3A46.330Z.json'
+  samples: 'https://pluspoolserverlessstack-databucketa7e4f76c-1o43g6j698ys.s3.us-east-2.amazonaws.com/2020-04-30T00_00_46.330Z.json'
 }
 
 const DIRECTIONS = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
