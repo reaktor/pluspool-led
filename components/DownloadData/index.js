4b5b4cc6bf33bb5005af8c7903e168239aee53c3
Anna <anna.harrington@reaktor.com>
2024-10-28 13:54:42 -0600

    csv download

--- components/DownloadData/index.js
+++ components/DownloadData/index.js
@@ -3,7 +3,11 @@ import { ENDPOINTS } from '../../helpers/constants'
 import styles from './DownloadData.module.css';
 
 const DownloadData = () => (
-  <a href={ENDPOINTS.samples} className={styles.container} aria-label='download data'>
+  <a
+    href={ENDPOINTS.samplesCsv}
+    className={styles.container}
+    aria-label='download data'
+  >
     <span className={styles.label}>Download Data</span>
     <span className={styles.icon}>
       <DownloadIcon />
--- helpers/constants.js
+++ helpers/constants.js
@@ -1,21 +1,21 @@
 const ENDPOINTS = {
-  testSamples:
-    'https://pluspoolserverlessstack-databucketa7e4f76c-1o43g6j698ys.s3.us-east-2.amazonaws.com/sampledata.json',
   samples:
     'https://pluspoolserverlessstack-databucketa7e4f76c-1o43g6j698ys.s3.us-east-2.amazonaws.com/samples.json',
+  samplesCsv:
+    'https://pluspoolserverlessstack-databucketa7e4f76c-1o43g6j698ys.s3.us-east-2.amazonaws.com/samples.csv',
 };
 
-const DIRECTIONS = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+const DIRECTIONS = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
 
-const BASE_URL = "https://water.pluspool.org";
+const BASE_URL = 'https://water.pluspool.org';
 
-const GA_TRACKING_ID = "UA-17668746-5";
+const GA_TRACKING_ID = 'UA-17668746-5';
 
 const DATE_UNITS = {
-  DAY: "day",
-  WEEK: "week",
-  MONTH: "month",
-  YEAR: "year",
+  DAY: 'day',
+  WEEK: 'week',
+  MONTH: 'month',
+  YEAR: 'year',
 };
 const DATA_DISCLAIMER =
   'Disclaimer: Data may have missing or incorrect values due to damaged, clogged or non-reporting sensors. Friends of + POOL makes every effort to remedy sensors onsite swiftly when data is not being logged.';
